7371bc5d9df3a92660635c3866f846bed4551b24
Michael Scofield <mscofield0@tutanota.com>
2024-10-18 02:50:54 +0200

    add DebugConsole component and fix

--- wu-tw/plugins/components/kbd.plugin.js
+++ wu-tw/plugins/components/kbd.plugin.js
@@ -12,7 +12,6 @@ export default ({ addComponents }) => {
 		'.kbd': {
 			'@apply inline-flex justify-center items-center': {},
 			'@apply border border-b-4 min-h-[2.2em] min-w-[2.2em] px-2 rounded-lg': {},
-			'@apply bg-surface-100 dark:bg-surface-800 border-surface-200 dark:border-surface-900': {},
 		},
 	});
 };
