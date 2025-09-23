238890ffeb9fe2486b92b0cc3879f74cc6a2b5d0
Michael Scofield <mscofield0@tutanota.com>
2025-09-23 17:27:17 +0200

    update table and is_route_in_set

--- src/components/table.rs
+++ src/components/table.rs
@@ -24,9 +24,15 @@ pub fn Table<T, E, Fut, F>(
 	/// Fallback (loading) view.
 	#[prop(optional, into)]
 	fallback: ViewFn,
+	/// Fallback view if the dataset is empty.
+	#[prop(optional, into)]
+	on_empty: ViewFn,
 	/// Corresponds to the 'class' attribute of elements.
 	#[prop(optional, into)]
 	class: Text,
+	/// Corresponds to the 'class' attribute of elements.
+	#[prop(optional, into)]
+	tbody_class: Text,
 	/// Corresponds to the 'style' attribute of elements.
 	#[prop(optional, into)]
 	style: Text,
@@ -41,76 +47,22 @@ where
 	let offset: RwSignal<u64> = RwSignal::new(0);
 	let data_resource = LocalResource::new(move || data_source(offset.get(), limit));
 
-	view! {
-		<div class=move || format!("wtable {class}") style=move || style.get().into_owned()>
-			<table>
-				<thead>
-					{move || header.run()}
-				</thead>
-				{move || match data_resource.get() {
-					Some(res) => Either::Left(match res {
-						Ok((total_count, records)) => Either::Left(view! {
-							<tbody>
-								{
-									let row = row.clone();
-									records
-										.into_iter()
-										.map(move |record| row.clone().run(record))
-										.collect::<Vec<_>>()
-								}
-							</tbody>
-							<tfoot>
-								<td class="grow center">
-									<div class="horizontal gap-4">
-										// Previous
-										<div class="flex center">
-											{move || match offset.get() != 0 {
-												false => Either::Left(view! { <div class="flex-none size-6 pointer-coarse:size-8"/> }),
-												true => Either::Right(view ! {
-													<button
-														on:click=move |_| offset.update(move |offset| *offset = offset.saturating_sub(1))
-														class="btn-icon autohighlight size-6 pointer-coarse:size-8"
-													>
-														<span class="icon i-o-arrow-left size-4" />
-													</button>
-												}),
-											}}
-										</div>
-										// Current pages
-										<div class="flex vcenter">
-											<span class="text-lg">
-												{move || offset.get() + 1}
-												" / "
-												{
-													let div = total_count.checked_div(limit).unwrap_or(0);
-													let has_rem = total_count.checked_rem(limit).map(|rem| (rem != 0) as u64).unwrap_or(1);
-													div + has_rem
-												}
-											</span>
-										</div>
-										// Next
-										<div class="flex center">
-											{move || match offset.get() < total_count.checked_div(limit).unwrap_or(0) {
-												false => Either::Left(view! { <div class="flex-none size-6 pointer-coarse:size-8"/> }),
-												true => Either::Right(view! {
-													<button
-														on:click=move |_| offset.update(move |offset| *offset = offset.saturating_add(1))
-														class="btn-icon autohighlight size-6 pointer-coarse:size-8"
-													>
-														<span class="icon i-o-arrow-right size-4" />
-													</button>
-												}),
-											}}
-										</div>
-									</div>
-								</td>
-							</tfoot>
-						}),
-						Err(err) => Either::Right(view! {
-							<tbody>
+	move || match data_resource.get() {
+		None => Either::Left(fallback.run()),
+		Some(res) => Either::Right(view! {
+			<div class=move || format!("wtable {class}") style=move || style.get().into_owned()>
+				<table>
+					<thead>
+						{let header = header.clone(); move || header.run()}
+					</thead>
+					{match res {
+						Err(err) => Either::Left(view! {
+							<tbody class=move || tbody_class.get()>
 								<div class="cover flex center">
 									<div class="vertical gap-2">
-										<span class="icon i-o-exclamation-triangle icon-error-500"/>
+										<div class="flex hcenter">
+											<span class="icon i-o-exclamation-triangle icon-error-500 icon-size-12"/>
+										</div>
 										<span class="font-semibold text-content-emph">
 											{format!("{err:?}")}
 										</span>
@@ -118,14 +70,74 @@ where
 								</div>
 							</tbody>
 						}),
-					}),
-					None => Either::Right(view! {
-						<tbody>
-							{fallback.run()}
-						</tbody>
-					}),
-				}}
-			</table>
-		</div>
+						Ok((total_count, records)) => Either::Right(match total_count == 0 {
+							true => Either::Left(view! {
+								<tbody class=move || tbody_class.get()>
+									{let on_empty = on_empty.clone(); move || on_empty.run()}
+								</tbody>
+							}),
+							false => Either::Right(view! {
+								<tbody class=move || tbody_class.get()>
+									{
+										let row = row.clone();
+										records
+											.into_iter()
+											.map(move |record| row.clone().run(record))
+											.collect::<Vec<_>>()
+									}
+								</tbody>
+								<tfoot>
+									<td class="grow center">
+										<div class="horizontal gap-4">
+											// Previous
+											<div class="flex center">
+												{move || match offset.get() != 0 {
+													false => Either::Left(view! { <div class="flex-none size-8"/> }),
+													true => Either::Right(view ! {
+														<button
+															on:click=move |_| offset.update(move |offset| *offset = offset.saturating_sub(1))
+															class="btn-icon autohighlight size-8"
+														>
+															<span class="icon i-o-arrow-left size-4" />
+														</button>
+													}),
+												}}
+											</div>
+											// Current pages
+											<div class="flex vcenter">
+												<span class="text-lg">
+													{move || offset.get() + 1}
+													" / "
+													{
+														let div = total_count.checked_div(limit).unwrap_or(0);
+														let has_rem = total_count.checked_rem(limit).map(|rem| (rem != 0) as u64).unwrap_or(1);
+														let result = div + has_rem;
+														(result == 0).then_some(1).unwrap_or(result)
+													}
+												</span>
+											</div>
+											// Next
+											<div class="flex center">
+												{move || match offset.get() < total_count.checked_div(limit).unwrap_or(0) {
+													false => Either::Left(view! { <div class="flex-none size-8"/> }),
+													true => Either::Right(view! {
+														<button
+															on:click=move |_| offset.update(move |offset| *offset = offset.saturating_add(1))
+															class="btn-icon autohighlight size-8"
+														>
+															<span class="icon i-o-arrow-right size-4" />
+														</button>
+													}),
+												}}
+											</div>
+										</div>
+									</td>
+								</tfoot>
+							}),
+						}),
+					}}
+				</table>
+			</div>
+		}),
 	}
 }
